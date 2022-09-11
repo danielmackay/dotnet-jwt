d488b814c906a542050a8736d7a997c2ce18cb3f
Daniel Mackay <daniel.mackay@live.com>
2022-09-12 07:23:29 +1000

    ✨ added login controller

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,69 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace dotnet_jwt.Controllers;
+
+[Route("/api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public LoginController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public ActionResult Login([FromBody] UserLogin userLogin)
+    {
+        var user = Authenticate(userLogin);
+        if (user != null)
+        {
+            var token = GenerateToken(user);
+            return Ok(token);
+        }
+
+        return NotFound("user not found");
+    }
+
+    private string GenerateToken(UserModel user)
+    {
+        var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new []
+        {
+            new Claim(ClaimTypes.NameIdentifier, user.Username),
+            new Claim(ClaimTypes.Role, user.Role)
+        };
+
+        var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+            _config["Jwt:Audience"],
+            claims,
+            null,                               // Note Before
+            DateTime.UtcNow.AddMinutes(15),     // Expiry, NOTE: May need to be local time
+            credentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserModel Authenticate(UserLogin userLogin)
+    {
+        var currentUser = UserConstants.Users
+            .FirstOrDefault(x => x.Username.ToLower() == userLogin.Username.ToLower()
+                && x.Password == userLogin.Password);
+
+            if (currentUser != null)
+            {
+                return currentUser;
+            }
+
+            return null;
+    }
+}
