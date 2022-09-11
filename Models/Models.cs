774d8eb5f1920879c15646e93c11e159bca64c2e
Daniel Mackay <daniel.mackay@live.com>
2022-09-12 07:07:52 +1000

    ✨ added models

---
+++ Models/Models.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+
+namespace dotnet_jwt;
+
+public record UserModel(string Username, string Password, string Role);
+
+public record UserLogin(string Username, string Password);
+
+public class UserConstants
+{
+    public static IReadOnlyList<UserModel> Users = new List<UserModel>()
+    {
+        new UserModel("daniel", "Password1?", "admin")
+    };
+}
